7083596329f7c7fc990fce487b8300930fd7bcd9
xunjezz <zeungeschris@gmail.com>
2022-06-27 15:18:11 -0500

    Remove finish function

--- js/javascript.js
+++ js/javascript.js
@@ -83,16 +83,3 @@ function reloadPage(){  //function to reload game
 }
 
 
-
-function finshGame(player,cpu){ //Display the winner in alert
-    if(player > cpu){
-        alert("You've Won! " + playerScore
-             + " to " + cpuScore +" Click Try Again!");
-    }else if(player < cpu){
-        alert("You've Lost! "+ playerScore
-        + " to " + cpuScore + " Click Try Again!");
-    }else if (player == cpu){
-        alert("It's a Draw! "+ playerScore
-        + " to " + cpuScore + " Click Try Again!");
-    }
-    }
