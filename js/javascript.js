78c07c283856e9c2cff6cc05274c06384c6a1a0d
xunjezz <zeungeschris@gmail.com>
2022-06-27 15:43:38 -0500

    Add div container for score elements, clean typos, Change element ID's to give better alert box

--- js/javascript.js
+++ js/javascript.js
@@ -4,34 +4,39 @@ let drawScore = 0;
 let gameScore = 1;
 let playerScore = 0;
 let cpu = 0;
-const taButton = document.createElement("button");
+const taButton = document.createElement('button');
         taButton.innerText = "Try Again?";
         taButton.addEventListener("click", reloadPage);
 
-const playerScoreCard = document.getElementById("ps");
+const container = document.getElementById('container');
+const playerScoreCard=document.createElement('p')
 playerScoreCard.innerText = `Player Score:  ${playerScore}`; //display scores and round
-const cpuScoreCard = document.getElementById("cpus");         
-cpuScoreCard.innerText = `CPU Score:  ${cpuScore}`; 
-const tieScoreCard = document.getElementById("ds");
+container.appendChild(playerScoreCard);
+const cpuScoreCard = document.createElement('p');         
+cpuScoreCard.innerText = `CPU Score:  ${cpuScore}`;
+container.appendChild(cpuScoreCard); 
+const tieScoreCard = document.createElement('p');
 tieScoreCard.innerText = ` Ties:  ${drawScore}`;
-const gameScoreCard = document.getElementById("gs"); 
-gameScoreCard.innerText = `Round:  ${gameScore}`;       
+container.appendChild(tieScoreCard);
+const gameScoreCard = document.createElement('p'); 
+gameScoreCard.innerText = `Round:  ${gameScore}`;
+container.appendChild(gameScoreCard);       
 
-clickedRock = document.querySelector('#click-rock'); //get click from input buttons
+clickedRock = document.querySelector('#Rock'); //get click from input buttons
 clickedRock.addEventListener('click', playerClicked);
-clickedPaper = document.querySelector('#click-paper');
+clickedPaper = document.querySelector('#Paper');
 clickedPaper.addEventListener('click', playerClicked);                        
-clickedScissors = document.querySelector('#click-scissors');
+clickedScissors = document.querySelector('#Scissors');
 clickedScissors.addEventListener('click', playerClicked);
 
 function playerClicked(e){
     computerPlay();
     if((gameScore >=5 && playerScore >= 3) && (playerScore > cpuScore) ){
-        gameScoreCard.innerHTML = 'GAME OVER <br>';
+        gameScoreCard.innerHTML = 'GAME OVER <br><br>';
         gameScoreCard.appendChild(taButton);
         alert(`GameOver You Win: ${playerScore} to ${cpuScore}, it's OVER! Click Try Again!`);
     }else if((gameScore>=5 && cpuScore >=3) && (cpuScore >  playerScore)){
-        gameScoreCard.innerHTML = 'GAME OVER <br>';
+        gameScoreCard.innerHTML = 'GAME OVER <br><br>';
         gameScoreCard.appendChild(taButton);
         alert(`GameOver You Lose: ${cpuScore} to ${playerScore}, it's OVER! Click Try Again!`);
     }else{
@@ -39,24 +44,24 @@ function playerClicked(e){
         if (e.target.id == cpu){
             alert(`Both you and your opponent chose ${e.target.id}`);
             tieScoreCard.innerText = `Ties:  ${drawScore += 1}`  
-        }else if(e.target.id == "click-rock" && cpu == "click-scissors"){
+        }else if(e.target.id == "Rock" && cpu == "Scissors"){
             alert("You've chosen Rock and your opponent has chosen Scissors, Win!!");
             playerScoreCard.innerText = `Player Score:  ${playerScore += 1}`
-        }else if(e.target.id == "click-rock" && cpu == "click-paper"){
+        }else if(e.target.id == "Rock" && cpu == "Paper"){
             alert("You've chosen Rock and unfortuatly your opponent has chosen Paper, Lose!");
             cpuScoreCard.innerText = `CPU Score:  ${cpuScore += 1}`;
-        }else if(e.target.id == "click-paper" && cpu == "click-rock"){
+        }else if(e.target.id == "Paper" && cpu == "Rock"){
             alert("You've chosen Paper and your opponent has chosen Rock, Win!!");
             playerScoreCard.innerText = `Player Score:  ${playerScore += 1}`
-        }else if(e.target.id == "click-paper" && cpu == "click-scissors"){
+        }else if(e.target.id == "Paper" && cpu == "Scissors"){
             alert("You've chosen Paper and unfortuatly your opponent has chosen Scissors, Lose!");
-            cpuScoreCard.innerText = `CPU Score:  ${cpuScore += 1};`
-        }else if(e.target.id == "click-scissors" && cpu == "click-paper"){
+            cpuScoreCard.innerText = `CPU Score:  ${cpuScore += 1}`
+        }else if(e.target.id == "Scissors" && cpu == "Paper"){
             alert("You've chosen Scissors and your opponent has chosen Paper, Win!!");
             playerScoreCard.innerText = `Player Score:  ${playerScore += 1}`
-        }else if(e.target.id == "click-scissors" && cpu == "click-rock"){
+        }else if(e.target.id == "Scissors" && cpu == "Rock"){
             alert("You've chosen Scissors and unfortuatly your opponent has chosen Rock, Lose!");
-            cpuScoreCard.innerText = `CPU Score:  ${cpuScore += 1};`
+            cpuScoreCard.innerText = `CPU Score:  ${cpuScore += 1}`
         }        
         
         gameScoreCard.innerText =` Round:  ${gameScore += 1}`;
@@ -69,11 +74,11 @@ function playerClicked(e){
 function computerPlay() {     //Generate random # between 1-3
     let randNum = Math.floor(Math.random() * 3 ) + 1;             
         if(randNum == 1){
-            cpu ="click-rock";       
+            cpu ="Rock";       
         }else if(randNum == 2){
-            cpu = "click-paper";
+            cpu = "Paper";
         }else if(randNum == 3){
-            cpu = "click-scissors";
+            cpu = "Scissors";
         }      
         return cpu;      
 }
